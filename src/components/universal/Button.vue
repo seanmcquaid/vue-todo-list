c6b9985045cd6a1170a9fdbbecc616e632990e62
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-26 23:34:59 -0400

    finished button component

---
+++ src/components/universal/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button type="type" @click="onClick"><slot /></button>
+</template>
+
+<script>
+export default {
+  props: {
+    onClick: { type: Function },
+    type: { type: String },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  border: 2px solid black;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Button :type="'button'" :onClick="click">Add To Do</Button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Button from '@/components/universal/Button.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Button,
+  },
+  data() {
+    return {
+      inputText: '',
+    };
+  },
+  methods: {
+    click() {
+      console.log('click');
+    },
   },
 };
 </script>
+
+<style scoped></style>
