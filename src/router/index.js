c9211e4fdb536c97582d2d0d1dfed427ecb2f0fa
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-29 16:30:56 -0400

    finished all tests

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import EditTodo from '../views/EditTodo.vue';
 
-const routes = [
+export const routes = [
   {
     path: '/',
     name: 'home',
--- tests/views/EditTodo.spec.js
+++ tests/views/EditTodo.spec.js
@@ -1,8 +1,46 @@
-import { fireEvent, screen } from '@testing-library/vue';
-import Home from '../../src/views/Home.vue';
+import { fireEvent, screen, waitFor } from '@testing-library/vue';
+import App from '../../src/App.vue';
+import { routes } from '../../src/router';
 import renderWithStoreAndRouter from '../../testUtils/renderWithStoreAndRouter';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('EditTodo', () => {
-  test('should ', () => {});
+  test('getTodoById', () => {
+    const state = {
+      todos: [{ text: 'oldTodo', isCompleted: false }],
+    };
+    renderWithStoreAndRouter(App, routes, state, '/edit/0');
+
+    expect(screen.getByText('oldTodo')).toBeInTheDocument();
+  });
+
+  test('submitEditedTodo', async () => {
+    const state = {
+      todos: [{ text: 'oldTodo', isCompleted: false }],
+    };
+    renderWithStoreAndRouter(App, routes, state, '/edit/0');
+
+    fireEvent.update(screen.getByTestId('inputText'), 'Update todo');
+
+    expect(screen.getByText('Update todo')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Submit Edit'));
+
+    await waitFor(() =>
+      expect(screen.getByText('Simple Todo List App')).toBeInTheDocument()
+    );
+
+    expect(screen.getByText('Update todo')).toBeInTheDocument();
+  });
+
+  test('textInputOnChange', () => {
+    const state = {
+      todos: [{ text: 'oldTodo', isCompleted: false }],
+    };
+    renderWithStoreAndRouter(App, routes, state, '/edit/0');
+
+    fireEvent.update(screen.getByTestId('inputText'), 'Update todo');
+
+    expect(screen.getByText('Update todo')).toBeInTheDocument();
+  });
 });
