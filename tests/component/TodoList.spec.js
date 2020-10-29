82a910ac4c5befd72847d839459523b33b1a16da
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-29 16:18:56 -0400

    finished todo list component tests

--- tests/component/TodoList.spec.js
+++ tests/component/TodoList.spec.js
@@ -4,7 +4,27 @@ import renderWithStore from '../../testUtils/renderWithStore';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('TodoList', () => {
-  test('Delete Todo', () => {});
+  test('Delete Todo', () => {
+    renderWithStore(TodoList, {
+      todos: [{ text: 'currentTodo', isCompleted: false }],
+    });
 
-  test('Complete Todo', () => {});
+    expect(screen.getByText('currentTodo')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('delete'));
+
+    expect(screen.queryByText('currentTodo')).toBeNull();
+  });
+
+  test('Complete Todo', () => {
+    renderWithStore(TodoList, {
+      todos: [{ text: 'currentTodo', isCompleted: false }],
+    });
+
+    expect(screen.getByText('currentTodo')).toBeInTheDocument();
+    expect(screen.getByText('currentTodo')).not.toHaveClass('isCompleted');
+
+    fireEvent.click(screen.getByText('currentTodo'));
+    expect(screen.getByText('currentTodo')).toHaveClass('isCompleted');
+  });
 });
